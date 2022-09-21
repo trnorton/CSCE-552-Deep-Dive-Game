6b6db00f00a7a316a4767f8bb606267b70baea8f
jdbraun1 <spsjbraun@gmail.com>
2022-09-20 23:41:03 -0400

    Added camera mobility on the X axis
    Made it so the camera follows the submarine when you move on the X axis as well as on the Y now

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,11 +6,18 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
     public GameObject bottom;
+    public GameObject sideLeft;
+    public GameObject sideRight;
     private float bottomY;
+    private float sideXpositive;
+    private float sideXnegative;
+
     // Start is called before the first frame update
     void Start()
     {
       bottomY = bottom.transform.position.y+5;
+      sideXpositive = sideLeft.transform.position.x+9;
+      sideXnegative = sideRight.transform.position.x-9;
     }
 
     // Update is called once per frame
@@ -18,6 +25,14 @@ public class CameraFollow : MonoBehaviour
     {
         if(player.transform.position.y > bottomY){
           transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+        }
+        if(player.transform.position.x > sideXpositive)
+        {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        }
+        if(player.transform.position.x < sideXnegative)
+        {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
         }
     }
 }
