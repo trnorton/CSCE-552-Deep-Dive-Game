f865a064558bb13ae6d617d70c49bb59dd12d8b7
Brandon Carter <bcarter9893@gmail.com>
2022-09-21 14:49:57 -0400

    Shark Idle Movement
    Shark will now move left and right if it is at its start location and no player is in its range.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -11,16 +11,26 @@ public class EnemyMovement : MonoBehaviour
     private GameObject sprite;
     public float distanceBetween = 7;
     private float distFromSpawn;
+    private Vector3 target;
+    private float startX;
+    private float startY;
+    public float idleEndPoint;
+    private float distIdle;
+
 
     void Start()
     {
         sprite = this.transform.Find("sharkUPLOADABLE").gameObject;
+        target = transform.position;
+        startX = transform.position.x;
+        startY = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
         sprite.GetComponent<SpriteRenderer>().flipY = true;
+        Debug.Log(startY);
         //From shark to player
         distance = Vector2.Distance((transform.position), player.transform.position);
         Vector2 direction = (player.transform.position) - (this.transform.position);
@@ -32,10 +42,15 @@ public class EnemyMovement : MonoBehaviour
         Vector2 directionToSpawn = (spawnPoint.transform.position) - (this.transform.position);
         directionToSpawn.Normalize();
         float angleToSpawn = (Mathf.Atan2(directionToSpawn.y, directionToSpawn.x) * Mathf.Rad2Deg) + 180;
+        
+        //For Idle movement
+        Vector2 directionIdle = target - this.transform.position;
+        float angleToIdle = (Mathf.Atan2(directionIdle.y, directionIdle.x) * Mathf.Rad2Deg) + 180;
 
         //If player is in range, shark chases
         if(distance < distanceBetween)
         {
+            target.x = startX;
             transform.position = Vector2.MoveTowards(this.transform.position, (player.transform.position), speed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(Vector3.forward * angle);
             if(direction[0] < 0) sprite.GetComponent<SpriteRenderer>().flipX = false;
@@ -43,15 +58,35 @@ public class EnemyMovement : MonoBehaviour
             if(direction[0] > 0) sprite.GetComponent<SpriteRenderer>().flipX = false;
             if(direction[0] > 0) sprite.GetComponent<SpriteRenderer>().flipY = true;
         }
-        //Player out of range, shark goes back to its original spot
-        else
+        //Player out of range, shark goes back to its original spot to go idle
+        else if (transform.position.y != startY)
         {
+            target.x = startX;
             transform.position = Vector2.MoveTowards(this.transform.position, (spawnPoint.transform.position), speed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(Vector3.forward * angleToSpawn);
             if(directionToSpawn[0] < 0) sprite.GetComponent<SpriteRenderer>().flipX = false;
             if(directionToSpawn[0] < 0) sprite.GetComponent<SpriteRenderer>().flipY = false;
             if(directionToSpawn[0] > 0) sprite.GetComponent<SpriteRenderer>().flipX = false;
             if(directionToSpawn[0] > 0) sprite.GetComponent<SpriteRenderer>().flipY = true;
+        }
+        //Idle
+        if (transform.position.y == startY)
+        {
+
+            if(transform.position.x == startX)
+            {
+                target.x = idleEndPoint;
+            }
+            else if(transform.position.x == idleEndPoint)
+            {
+                target.x = startX;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angleToIdle);
+            if(directionIdle[0] < 0) sprite.GetComponent<SpriteRenderer>().flipX = false;
+            if(directionIdle[0] < 0) sprite.GetComponent<SpriteRenderer>().flipY = false;
+            if(directionIdle[0] > 0) sprite.GetComponent<SpriteRenderer>().flipX = false;
+            if(directionIdle[0] > 0) sprite.GetComponent<SpriteRenderer>().flipY = true;
         }
     }
 
