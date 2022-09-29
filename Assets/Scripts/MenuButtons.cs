dc26c4e96f93eafd98261f38907674e86561231e
Taylor Norton <taylornorton@Taylors-MacBook-Pro-3.local>
2022-09-28 22:33:24 -0400

    Added level 1 - jellyfish

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -8,17 +8,17 @@ public class MenuButtons : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void OnClickStart()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Level1");
     }
     public void OnClickQuit()
     {
